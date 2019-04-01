8590602a96da91762108aa83ed9c43f9bafcc7b5
sjavvaa <idm.sjavvaa@yandex.ru>
2019-04-01 12:46:12 +0300

    New System command

--- INIT_VARIABLE.v
+++ INIT_VARIABLE.v
@@ -11,42 +11,58 @@
 //			--- CORE ---
 // COMMANDs SYSTEM					OP1	OP2	ALU	IP			flag	stack	
 `define	COM_NOP		8'd0		//	x		x		x		+			x		x
-`define	COM_ADD		8'd1		//	x		x		0		+			x		x
-`define	COM_SUB		8'd2		//	x		x		1		+			x		x
-`define	COM_EOR		8'd3		//	x		x		2		+			x		x
-`define	COM_EAND		8'd4		//	x		x		3		+			x		x
-`define	COM_OR		8'd5		//	x		x		4		+			x		x
-`define	COM_AND		8'd6		//	x		x		5		+			x		x
-`define	COM_NOT		8'd7		//	x		x		6		+			x		x
-`define	COM_XOR		8'd8		//	x		x		7		+			x		x
-`define	COM_LSH		8'd9		//	x		x		8		+			x		x
-`define	COM_RSH		8'd10		//	x		x		9		+			x		x
-`define	COM_CP		8'd11		//	x		x		10		+			x		x
-`define	COM_MOV		8'd12		//	A<----B		x		+			x		x
+//------------------------------------------------------------------- ALU
+`define	COM_ADD		8'd1		//	x		x		0		+			x		x		+
+`define	COM_SUB		8'd2		//	x		x		1		+			x		x		-
+`define	COM_EOR		8'd3		//	x		x		2		+			x		x		||
+`define	COM_EAND		8'd4		//	x		x		3		+			x		x		&&
+`define	COM_OR		8'd5		//	x		x		4		+			x		x		|
+`define	COM_AND		8'd6		//	x		x		5		+			x		x		&
+`define	COM_NOT		8'd7		//	x		x		6		+			x		x		!
+`define	COM_XOR		8'd8		//	x		x		7		+			x		x		^
+`define	COM_LSH		8'd9		//	x		x		8		+			x		x		<<
+`define	COM_RSH		8'd10		//	x		x		9		+			x		x		>>
+`define	COM_CP		8'd11		//	x		x		10		+			x		x		-
+//------------------------------------------------------------------- REG
+`define	COM_MOV		8'd12		//	A<----B		x		+			x		x		
 `define	COM_LDI		8'd13		//	DX<----		x		+			x		x
-`define	COM_BREQ		8'd14		//	{const}		x		new ip/+	x		x
-`define	COM_BRNE		8'd15		//	{const}		x		new ip/+	x		x
-`define	COM_BRCS		8'd16		//	{const}		x		new ip/+	x		x
-`define	COM_BRCC		8'd17		//	{const}		x		new ip/+	x		x
-`define	COM_L2MEM	8'd18		//	{const}		x		+			x		x
-`define	COM_JMP		8'd19		//	{const}		x		new ip	x		x
-`define	COM_PUSH		8'd20		//	stk<--A		x		+			x		+
-`define 	COM_POP		8'd21		//	A<--stk		x		+			x		-
-`define	COM_CALL		8'd22		//	stk<{fl,ip}	x		+			x		+
-`define	COM_RET		8'd23		//	{fl,ip}<stk	x		+			x		-	
-`define	COM_INT		8'd24		//			
-`define	COM_INTON	8'd25		//	
-`define	COM_INTOFF	8'd26		//
-`define	COM_SET		8'd27		//
-`define	COM_GET		8'd28		//
-`define	COM_CORE		8'd29		//
-`define	COM_SET_SA	8'd29
-`define	COM_SET_SB	8'd29
-`define	COM_SET_SC	8'd29
-`define	COM_GET_SA	8'd29
-`define	COM_GET_SB	8'd29
-`define	COM_GET_SC	8'd29
-`define	COM_STORE	8'd29
+//------------------------------------------------------------------- JMP
+`define	COM_BREQ		8'd14		//	{const}		x		new ip/+	x		x		fZ				
+`define	COM_BRNE		8'd15		//	{const}		x		new ip/+	x		x		!fZ
+`define	COM_BRCS		8'd16		//	{const}		x		new ip/+	x		x		fC
+`define	COM_BRCC		8'd17		//	{const}		x		new ip/+	x		x		!fC
+`define	COM_JMP		8'd18		//	{const}		x		new ip	x		x	
+//------------------------------------------------------------------- SET ADDR from REG for all
+`define	COM_SET		8'd19		//	A<----B		x		+			x		x	
+//------------------------------------------------------------------- L2
+`define	COM_L2MEM	8'd20		//	{const}		x		+			x		x		set addr
+//------------------------------------------------------------------- L3
+`define	COM_CORE		8'd21		// {const}		x		+			x		x		set core
+`define	COM_L3MEM	8'd22		//	{const}		x		+			x		x		set addr
+//------------------------------------------------------------------- FLAG
+// automate for ALU comands and stack
+//------------------------------------------------------------------- STACK
+`define	COM_PUSH		8'd23		//	stk<--A		x		+			x		+
+`define 	COM_POP		8'd24		//	A<--stk		x		+			x		-
+`define	COM_CALL		8'd25		//	stk<{fl,ip}	x		+			x		+
+`define	COM_RET		8'd26		//	{fl,ip}<stk	x		+			x		-	
+//------------------------------------------------------------------- INT
+`define	COM_INT		8'd27		//	{const}		x		+			x		x		init programm int	
+`define	COM_INTON	8'd28		//	{const}		x		+			x		x		int
+`define	COM_INTOFF	8'd29		//	{const}		x		+			x		x		int
+//------------------------------------------------------------------- STORE 
+`define	COM_SET_SA	8'd30		//	SA<---B		x		+			x		x		set segment [A,
+`define	COM_SET_SB	8'd31		//	SB<---B		x		+			x		x		set segment ,B,
+`define	COM_SET_SC	8'd32		//	SC<---B		x		+			x		x		set segment ,C,IP]
+`define	COM_GET_SA	8'd33		//	B<---SA		x		+			x		x		get segment [A,
+`define	COM_GET_SB	8'd34		//	B<---SB		x		+			x		x		get segment ,B,
+`define	COM_GET_SC	8'd35		//	B<---SC		x		+			x		x		get segment ,C,IP]
+`define	COM_SAVE		8'd36		//	x				x		+			x		x		save data from L4 to MEM
+`define	COM_LOAD		8'd37		//	x				x		+			x		x		load data from MEM to L4
+//------------------------------------------------------------------- SDRAM 
+`define	COM_SDSAVE	8'd38		//	x				x		+			x		x		save data from L4 to SDRAM
+`define	COM_SDLOAD	8'd39		//	x				x		+			x		x		load data from SDRAM to L4
+	
 
 
 
@@ -57,6 +73,10 @@
 `define	ADR_DX		4'd3
 `define	ADR_ACC		4'd4
 `define	ADR_L2MEM	4'd5
+`define 	ADR_FLAG		4'd6
+`define	ADR_STACK	4'd7
+`define	ADR_L3MEM	4'd8
+`define	ADR_INT		4'd9
 
 
 //			--- ALU ---
